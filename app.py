8305c8897157e203bc9fd1d8f1944973e1bbdede
GAURAV SINGH <gaurav7jpr@gmail.com>
2021-08-13 15:33:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ from fbprophet import Prophet
 from fbprophet.plot import plot_plotly
 from plotly import graph_objs as go
 
-START = "2015-01-01"
+START = "2019-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
 
 st.title('Stock Forecast App')
 
-stocks = ('GOOG', 'AAPL', 'MSFT', 'FB','CSCO','QCOM','SBUX','TSLA')
+stocks = ('GOOG', 'AAPL')
 selected_stock = st.selectbox('Select dataset for prediction', stocks)
 
 n_years = st.slider('Years of prediction:', 1, 4)
